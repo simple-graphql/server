95d2a65040f3fa7e547fd271aba46538c3b4fdab
Mohamed Tawfik <motawfik98@outlook.com>
2021-08-02 20:27:34 +0200

    added initial graphql schema

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
 
 const app = express();
+
+app.use("/graphql", graphqlHTTP({
+  schema // schema: schema - as both names are the same
+}));
 
 app.listen(4000, ()=> {
   console.log("Listening for requests on port 4000")
