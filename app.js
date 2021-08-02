f8aecef82c286bb942272d49b20355cae647e212
Mohamed Tawfik <motawfik98@outlook.com>
2021-08-02 21:51:47 +0200

    added db connection

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const mysql = require("mysql")
+require("dotenv").config()
 
 const app = express();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log("Error connecting to DB: " + err.stack);
+    return;
+  }
+  console.log("connected successfully to DB (ID: " + connection.threadId + ")");
+})
 
 app.use("/graphql", graphqlHTTP({
   schema, // schema: schema - as both names are the same
